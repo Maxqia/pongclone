599452488d6f451ecfa96cdab527bd125169ef84
Max Qian <public@maxqia.com>
2017-06-10 20:44:51 -0700

    Bouncy Balls!

--- game.js
+++ game.js
@@ -5,7 +5,10 @@ const y_size = 300;
 const paddle_length = 50;
 const paddle_width = 15;
 const paddle_distance_edge = 40;
+const paddle_move_velocity = 4;
 const ball_size = 4;
+const ball_y_velocity = 4;
+const ball_x_velocity = 1;
 // -- end constants --
 
 canvas = document.getElementById('mainCanvas');
@@ -13,10 +16,14 @@ canvas.width = x_size;
 canvas.height = y_size;
 
 ctx = canvas.getContext("2d");
+pause = false;
 pad1y = pad2y = (y_size - paddle_length) / 2 // top of the paddle (in center)
-pad1grav = pad2grav = ballXgrav = ballYgrav = 0;
-ballXPos = (x_size - ball_size) / 2
-ballYPos = (y_size - ball_size) / 2
+pad1grav = pad2grav = 0;
+ballXpos = (x_size - ball_size) / 2
+ballYpos = (y_size - ball_size) / 2
+ballXgrav = Math.random() >= 0.5 ? ball_x_velocity : -ball_x_velocity;
+          //(Math.random() * 2) - 1;
+ballYgrav = ball_y_velocity;
 
 window.requestAnimationFrame(gameTick);
 function gameTick() {
@@ -29,11 +36,26 @@ function gameTick() {
   drawPaddle(false, pad1y);
   drawPaddle(true, pad2y);
   drawBall();
-  window.requestAnimationFrame(gameTick);
+  if (!pause) window.requestAnimationFrame(gameTick);
 }
 
 function drawBall() {
-  ctx.fillRect(ballXPos, ballYPos, ball_size, ball_size);
+  ballXpos += ballXgrav;
+  ballYpos += ballYgrav;
+
+  // roll over the sides
+  if (ballXpos <= 0 - ball_size) ballXpos += (x_size + ball_size);
+  if (ballXpos >= x_size + ball_size) ballXpos -= (x_size + ball_size);
+  // switch y direction if it hits the ceiling
+  if (ballYpos <= 0 || ballYpos >= (y_size - ball_size)) {
+    ballYgrav *= -1;
+    /*ballXpos += ballXgrav;
+    ballYpos += ballYgrav;*/
+    //pause = true;
+  }
+
+
+  ctx.fillRect(ballXpos, ballYpos, ball_size, ball_size);
 }
 
 function drawPaddle(right, yPos) {
@@ -48,16 +70,16 @@ function drawPaddle(right, yPos) {
 document.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
     case W :
-        pad1grav = -2;
+        pad1grav = -paddle_move_velocity;
         break;
     case S :
-        pad1grav = 2;
+        pad1grav = paddle_move_velocity;
         break;
     case UP :
-        pad2grav = -2;
+        pad2grav = -paddle_move_velocity;
         break;
     case DOWN :
-        pad2grav = 2;
+        pad2grav = paddle_move_velocity;
         break;
   }
 });
